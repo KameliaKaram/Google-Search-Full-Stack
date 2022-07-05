be4329b86e3409ac451b84f237faa26c38e8f2fb
Kamelia Karam <kamelia_karam@hotmail.com>
2022-07-05 14:19:53 +0700

    replaced results with handlebars

--- controllers/results.js
+++ controllers/results.js
@@ -4,8 +4,49 @@ const router = express.Router()
 
 // Create POST controller
 router.get ('/', (req, res) => {
-    res.render ('results')
+    let results = [{
+        title: 'JS tutorials',
+        description: 'The best JavaScript tutorials in the galaxy!',
+        url: 'https://www.w3schools.com',
+        links: [
+            {
+            title: 'JS for Beginners',
+            url: 'https://www.w3schools.com/js'
+            }, {
+            title: 'JS for the Web',
+            url: 'https://www.w3schools.com/js'
+            }
+        ]
+    }, {
+        title: 'JavaScript | MDN',
+        description: 'JavaScript (JS) is a lightweight' ,
+        url: 'https://www.w3schools.org',
+        links: [
+            {
+            title: 'JS 1',
+            url: 'https://www.w3schools.org/js'
+            }, {
+            title: 'JS 2',
+            url: 'https://www.w3schools.org/js'
+            }
+        ]
+    }, {
+        title: 'JavaScript - Wikipedia',
+        description: 'The best JavaScript tutorials in the galaxy!',
+        url: 'https://www.w3schools.net',
+        links: [
+            {
+            title: 'JavaScript Online',
+            url: 'https://www.w3schools.com/js'
+            }, {
+            title: 'JavaScript Definition',
+            url: 'https://www.w3schools.com/js'
+            }
+        ]
+    }]
+    res.render ('results', {results})
 })
+
 
 // Export module
 module.exports = router
